77831b413de9f1009b10d32b7024e008813cf4e4
odyn.owl <odyn.owl@gmail.com>
2009-07-18 16:40:40 +0300

    use before construction in tests

--- apple_spec.rb
+++ apple_spec.rb
@@ -17,23 +17,23 @@ describe Apple, "when first created" do
   
   include AppleHelperMethods
 
-  it "should size 0 for new apple" do
+  before do
     create_new_apple
+  end
+
+  it "should size 0 for new apple" do    
     @apple.size.should == 0
   end
   
-  it "should age 0 for new apple" do
-    create_new_apple
+  it "should age 0 for new apple" do    
     @apple.age.should == 0
   end
 
   it "should color green for new apple" do
-    create_new_apple
     @apple.color.should == 'green'
   end
 
   it "should new apple don't be ready for eating" do
-    create_new_apple
     @apple.ready?.should == false
   end
 
@@ -45,11 +45,20 @@ describe Apple, "when it is growing" do
  
   include AppleHelperMethods
 
-  it "should apple age is growing" do
+  before do
     create_new_apple
+  end
+  
+  it "should apple age is growing" do
     @apple.grow(20)
     @apple.age.should == 20
   end
-
+  
+  it "should apple is have to have bigger size" do
+    create_new_apple
+    size_prev = @apple.size
+    @apple.grow(100)
+    @apple.size.should > size_prev
+  end
 end
 
