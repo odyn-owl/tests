24dd403ac4261c2b709eec99714416f8a9d4972d
Andriy Yanko <andriy.yanko@gmail.com>
2009-07-19 13:58:18 +0300

    DRY apple spec -  Do NOT repeat yourself!

--- spec/apple_spec.rb
+++ spec/apple_spec.rb
@@ -1,51 +1,50 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Apple, "when first created" do
+describe Apple do
   
   include AppleSpecHelper
-
+  
   before do
     create_new_apple
   end
 
-  it "should size 0 for new apple" do    
-    @apple.size.should == 0
+
+
+  describe "when first created" do
+
+    it "should size 0 for new apple" do    
+      @apple.size.should == 0
+    end
+
+    it "should age 0 for new apple" do    
+      @apple.age.should == 0
+    end
+
+    it "should color green for new apple" do
+      @apple.color.should == 'green'
+    end
+
+    it "should new apple don't be ready for eating" do
+      @apple.ready?.should == false
+    end
+
   end
+
+
+
+  describe "when it is growing" do
   
-  it "should age 0 for new apple" do    
-    @apple.age.should == 0
-  end
+    it "should apple age is growing" do
+      @apple.grow(20)
+      @apple.age.should == 20
+    end
 
-  it "should color green for new apple" do
-    @apple.color.should == 'green'
-  end
-
-  it "should new apple don't be ready for eating" do
-    @apple.ready?.should == false
+    it "should apple is have to have bigger size" do
+      create_new_apple
+      size_prev = @apple.size
+      @apple.grow(100)
+      @apple.size.should > size_prev
+    end
   end
 
 end
-
-
-
-describe Apple, "when it is growing" do
- 
-  include AppleSpecHelper
-
-  before do
-    create_new_apple
-  end
-  
-  it "should apple age is growing" do
-    @apple.grow(20)
-    @apple.age.should == 20
-  end
-  
-  it "should apple is have to have bigger size" do
-    create_new_apple
-    size_prev = @apple.size
-    @apple.grow(100)
-    @apple.size.should > size_prev
-  end
-end
-
